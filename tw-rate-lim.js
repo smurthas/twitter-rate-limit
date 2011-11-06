0f8a7c475af4805bc9d34b6a92fafdc6c49d9722
Simon Murtha-Smith <simon@murtha-smith.com>
2011-11-06 13:43:37 -0800

    hello, rate limit!

---
+++ tw-rate-lim.js
@@ -0,0 +1,20 @@
+if(process.argv[2]) {
+    var auth = JSON.parse(require('fs').readFileSync(process.argv[2]));
+    require('twitter-js')(auth.consumerKey, auth.consumerSecret).apiCall('GET', '/account/rate_limit_status.json', 
+                            { token: { oauth_token_secret: auth.token.oauth_token_secret, oauth_token: auth.token.oauth_token}},
+        function(error, rl) {
+            if(error) console.error(error);
+            else {
+                rl.mins_remaining = Math.round((rl.reset_time_in_seconds - new Date().getTime()/1000)/ 6) / 10;
+                console.log(rl);
+            } 
+        });
+} else {
+    require('request').get({uri:'http://api.twitter.com/1/account/rate_limit_status.json', json:true}, function(err, resp, rl) {
+        if(err) console.error(err);
+        else {
+            rl.mins_remaining = Math.round((rl.reset_time_in_seconds - new Date().getTime()/1000)/ 6) / 10;
+            console.log(rl);
+        }
+    });
+}
